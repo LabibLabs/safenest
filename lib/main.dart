b5cdb66df5a39aa8e21cc67ba68f17d04293377a
Labib Mamun <labibmamun05@gmail.com>
2026-09-11 22:19:03 +0600

    Firebase code in main file. Version 2.6.4

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
-import 'screen/privacy_and_permissions_screen.dart';
-void main()
-{
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:safenest/screen/privacy_and_permissions_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:PrivacyAndPermissionScreen(),
-    )
+      home: PrivacyAndPermissionScreen(),
+    ),
   );
 }
